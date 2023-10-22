d1b66bdaf8dd6175a800d2a3c49a268d6e36909e
Bishal Giri <bishal0922@gmail.com>
2023-10-21 21:25:26 -0500

    complete form + export json

--- src/components/PortfolioForm.jsx
+++ src/components/PortfolioForm.jsx
@@ -1,7 +1,35 @@
 import React, { useState } from "react";
-import { Button, TextField, Container, Typography, Box, CircularProgress, Snackbar, Alert, Select, MenuItem } from "@mui/material";
+import { Button, TextField, Container, Typography, Box, CircularProgress, Snackbar, Alert, Select, MenuItem, Paper } from "@mui/material";
+import { styled } from '@mui/system';
+
+const StyledContainer = styled(Container)(({ theme }) => ({
+    padding: theme.spacing(4),
+    borderRadius: '8px',
+}));
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#ffffff',  // Pure white to contrast against the softer background
+    padding: theme.spacing(2),
+    marginBottom: theme.spacing(3),
+    boxShadow: '0 2px 4px 0 rgba(0, 0, 0, .08)'  // Minimal shadow for a subtle lift effect
+}));
+
+const HeaderTypography = styled(Typography)(({ theme }) => ({
+    color: '#333',  // Dark grey for headers
+    marginBottom: theme.spacing(2),
+    fontWeight: '500'
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+    backgroundColor: '#667eea',  // Soft blue for call-to-action buttons
+    color: 'white',
+    '&:hover': {
+        backgroundColor: '#5a6fcf'  // A slightly darker blue for hover
+    }
+}));
 
 const PortfolioForm = () => {
+    
   const [formData, setFormData] = useState({
     name: "",
     experiences: [
@@ -163,13 +191,43 @@ const addLink = () => {
 PERSONAL LINKS ENDS 
 */
 
+const handleGenerateClick = (e) => {
+    // For now, let's just log the data to the console
+    console.log("wassup foo");
+    e.preventDefault();  // Prevent form submission
+
+    setLoading(true);
+
+    // Simulating a delay for generating. 
+    // Later on, this can be replaced with actual generation logic.
+    setTimeout(() => {
+        const jsonData = JSON.stringify(formData, null, 2);
+        const blob = new Blob([jsonData], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+
+        // Opens in new tab
+        window.open(url, '_blank');
+        
+        setLoading(false);
+        setSuccess(true);  // Show success snackbar
+    }, 1000);  // 2 seconds delay, adjust as needed
+};
+
+const handleKeyDown = (e) => {
+    // Check if the "Enter" key was pressed
+    if (e.key === 'Enter') {
+        e.preventDefault();
+    }
+};
+
+
 
   return (
-    <Container>
+    <StyledContainer>
       <Typography variant="h4" gutterBottom>
         Portfolio Generator
       </Typography>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onKeyDown={handleKeyDown}>
         <Box mb={2}>
           <TextField
             fullWidth
@@ -178,12 +236,15 @@ PERSONAL LINKS ENDS
             name="name"
             value={formData.name}
             onChange={handleChange}
-            error={error === "Name is required"}
-            helperText={error === "Name is required" && error}
           />
         </Box>
 
+
         {/* Insert the Experience Rendering Here */}
+         <StyledPaper elevation={3} style={{ padding: '15px', marginBottom: '20px' }}>
+                <HeaderTypography variant="h6" gutterBottom>
+                    Experience
+                </HeaderTypography>
         {formData.experiences.map((experience, index) => (
           <Box key={index} mb={2}>
             <TextField
@@ -204,19 +265,17 @@ PERSONAL LINKS ENDS
         >
           Add More Experience
         </Button>{" "}
-        <Box mb={2}>
-          <TextField
-            fullWidth
-            label="Experience"
-            variant="outlined"
-            name="experience"
-            value={formData.experience}
-            onChange={handleChange}
-          />
-        </Box>
+        </StyledPaper>
+
+
+ 
 
         {/* PROJECTS HERE */}
       {/* Render Projects */}
+      <StyledPaper elevation={3} style={{ padding: '15px', marginBottom: '20px' }}>
+                <HeaderTypography variant="h6" gutterBottom>
+                    Projects
+                </HeaderTypography>
 {formData.projects.map((project, index) => (
     <Box key={index} mb={2}>
         <TextField
@@ -245,9 +304,15 @@ PERSONAL LINKS ENDS
 <Button onClick={addProject} variant="outlined" style={{ marginRight: '10px' }}>
     Add Another Project
 </Button>
+</StyledPaper>
+
 
       {/* personal links */}
       {/* Render Social Media Links */}
+        <StyledPaper elevation={3} style={{ padding: '15px', marginBottom: '20px' }}>
+                <HeaderTypography variant="h6" gutterBottom>
+                    Personal Links
+                </HeaderTypography>
 {formData.personalLinks.map((link, index) => (
     <Box key={index} mb={2} display="flex" alignItems="center" justifyContent="space-between">
         <Select
@@ -278,17 +343,20 @@ PERSONAL LINKS ENDS
 <Button onClick={addLink} variant="outlined">
     Add Another Link
 </Button>
+</StyledPaper>
+
 <br></br>
 
-        <Button
+        <StyledButton
           type="submit"
           variant="contained"
           color="primary"
           disabled={loading}
           style={{ marginTop: "20px" }}
+          onClick={handleGenerateClick}
         >
           {loading ? <CircularProgress size={24} /> : "Generate Portfolio"}
-        </Button>
+        </StyledButton>
       </form>
       <Snackbar
         open={success}
@@ -303,7 +371,8 @@ PERSONAL LINKS ENDS
           Portfolio generated successfully!
         </Alert>
       </Snackbar>
-    </Container>
+    </StyledContainer>
+
   );
 };
 
